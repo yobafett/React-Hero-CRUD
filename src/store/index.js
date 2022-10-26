45458c13502e20efcbad37226da7798fd7a207c0
Yoba Fett <alexka.680@gmail.com>
2022-10-26 20:37:42 +0900

    Use RTK Querry for heroes

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import heroes from '../components/heroesList/heroesSlice';
 import filters from '../components/heroesFilters/filterSlice';
+import { apiSlice } from '../api/apiSlice';
 
 const stringMiddleware = () => (next) => (action) => {
     if (typeof action === 'string') {
@@ -13,10 +13,10 @@ const stringMiddleware = () => (next) => (action) => {
 
 const store = configureStore({
     reducer: {
-        heroes: heroes,
-        filters: filters
+        filters: filters,
+        [apiSlice.reducerPath]: apiSlice.reducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(stringMiddleware),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(stringMiddleware, apiSlice.middleware),
     devTools: process.env.NODE_ENV !== "production",
 });
 
